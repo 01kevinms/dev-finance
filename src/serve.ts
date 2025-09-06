33dfa39e2c456f7ac9c4e091aae369d35bb40423
01kevinms <kevin.victors29@gmail.com>
2025-09-06 15:36:19 -0300

    backend

--- src/serve.ts
+++ src/serve.ts
@@ -3,8 +3,9 @@ import { env } from "./config/env";
 import initializeFirebaseAdmin from "./config/firebase";
 import { prismaConnect } from "./config/prisma";
 import { initializeGlobalCategories } from "./services/globalCategories.services";
+import { FastifyListenOptions } from "fastify";
 
-// Garantir que PORT seja número e usar padrão caso não exista
+// Garantir que PORT seja número
 const PORT = Number(env.PORT) || 3333;
 
 // Inicializar Firebase
@@ -20,12 +21,17 @@ const startServe = async () => {
     await initializeGlobalCategories();
     console.log("Categorias globais inicializadas ✅");
 
-    // Start do servidor escutando em 0.0.0.0 (obrigatório para Render)
-    await app.listen({ port: PORT, host: "0.0.0.0" });
+    // Configuração do Fastify para Render
+    const listenOptions: FastifyListenOptions = {
+      port: PORT,
+      host: "0.0.0.0", // obrigatório para Render
+    };
+
+    await app.listen(listenOptions);
     console.log(`Server is running on port ${PORT}`);
   } catch (err) {
     console.error("Erro ao iniciar o servidor:", err);
-    process.exit(1); // Finaliza o processo se houver erro
+    process.exit(1);
   }
 };
 
